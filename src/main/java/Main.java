7d9dc8787265a35d44e9a402be0ed83ffa7426fa
willian <willian365x@gmail.com>
2025-02-11 00:56:19 -0300

    Use Scanner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Put your name:");
+        String nome = scanner.nextLine();
+        System.out.printf("User name %s contains %d caracteres %n", nome.toLowerCase(), nome.length());
+    }
 }
